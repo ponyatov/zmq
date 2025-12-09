89524be433d7d0ece6ec3c95ea0c41d00f31aa95
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-09 10:22:18 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# zmq
-cppzmq + pcpp (DPDK)
+# MultithreadedGeneratorUDP
+## генератор высокоскоростного UDP-трафика
+### zmq: cppzmq + pcpp (DPDK)
+
+task: https://tracker.yandex.ru/ECOLITESOFTT-150
 
 git remote add flic git@gitflic.ru:dponyatov/zmq.git
+
+- поток до 100...200 Gbit
+- UDP трафик на порты 40000..40024
---
+++ legas/client/client.ml
@@ -0,0 +1,16 @@
+let app = "MultithreadedGeneratorUDP"
+
+let title = "генератор высокоскоростного UDP-трафика"
+
+let subtitle = "zmq: cppzmq + pcpp (DPDK)"
+
+let about = "- поток до 100...200 Gbit
+- UDP трафик на порты 40000..40024
+"
+
+let remote =
+  [
+    ("flic", "git@gitflic.ru:dponyatov/multithreadedgeneratorudp.git");
+    ("gh", "git@github.com:ponyatov/MultithreadedGeneratorUDP.git");
+    ("origin", "https://github.com/Nicktonious/MultithreadedGeneratorUDP.git");
+  ]
